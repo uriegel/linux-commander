cc27430843990f9e7ac198b8ef270fddfc18797b
uriegel <uriegel@hotmail.de>
2021-05-02 17:53:13 +0200

    GTK added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # commander
 A Norton Commander clone based on Web Components and Rust
+## Prerequisites on Linux (Fedora)
+* ```sudo dnf install gtk3-devel```
+* ```sudo dnf install libsoup-devel```
+* ```sudo dnf install webkit2gtk3-devel.x86_64```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate gtk;
+extern crate gio;
+
+use gio::{ActionMapExt, ApplicationFlags, prelude::ApplicationExt};
+use glib::clone;
+use gtk::{Application, GtkApplicationExt};
+
 fn main() {
-    println!("Hello, world!");
+    let application = Application::new(Some("de.uriegel.commander"), ApplicationFlags::empty())
+        .expect("Application::new() failed");
+
+    let action = gio::SimpleAction::new("destroy", None);
+    action.connect_activate(clone!(@weak application => move |_,_| application.quit()));
+    application.add_action(&action);
+    application.set_accels_for_action("app.destroy", &["<Ctrl>Q"]);
+    
 }
