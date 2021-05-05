86c10c38a4ff9cc24a9cecc893da93153777267e
uriegel <uriegel@hotmail.de>
2021-05-05 07:41:46 +0200

    settings if file does not exits, but directory

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,47 @@
-localStorage.setItem("Test", "gut")
+import './components/gridsplitter.js'
+import { VERTICAL, HORIZONTAL } from '/components/gridsplitter.js'
+
+const themeChooser = document.getElementById("themeChooser")
+const gridChooser = document.getElementById("gridChooser")
+const secondInvisible = document.getElementById("secondInvisible")
+const splitter = document.querySelector('grid-splitter')
+
+themeChooser.onchange = () => {
+    const changeTheme = theme => {
+        ["themeBlue", "themeAdwaita", "themeAdwaitaDark"].forEach(n => {
+            document.body.classList.remove(n)    
+            table.classList.remove(n)    
+        })
+        document.body.classList.add(theme)    
+        const style = getComputedStyle(document.body)
+        exifColor = style.getPropertyValue('--exif-color') 
+        selectedExifColor = style.getPropertyValue('--selected-exif-color') 
+        table.classList.add(theme)    
+        table.themeChanged()
+    }
+
+    switch (themeChooser.selectedIndex) {
+        case 0: 
+            changeTheme("themeBlue")
+            break
+        case 1: 
+            changeTheme("themeAdwaita")
+            break
+        case 2: 
+            changeTheme("themeAdwaitaDark")
+            break
+    }
+}
+
+gridChooser.onchange = () => {
+    switch (gridChooser.selectedIndex) {
+        case 0: 
+            splitter.setAttribute("orientation", HORIZONTAL)
+            break
+        case 1: 
+            splitter.setAttribute("orientation", VERTICAL)
+            break
+    }
+}
+
+secondInvisible.onchange = () => splitter.setAttribute("secondInvisible", secondInvisible.checked)
--- src/settings.rs
+++ src/settings.rs
@@ -1,6 +1,5 @@
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
-use std::{fs, fs::File, io::ErrorKind, path::{PathBuf}};
+use std::{fs, fs::{File}, io::{ErrorKind, Read, Write}, path::{PathBuf}};
 
 #[derive(Serialize, Deserialize)]
 pub struct Settings {
@@ -10,29 +9,34 @@ pub struct Settings {
 
 pub fn initialize() -> Settings {
     let settings = get_settings_path();
+    println!("{:?}",settings);
     match File::open(settings) {
-        Ok(file) => {
-            Settings { width: 0, height: 0 }
+        Ok(mut file) => {   
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).unwrap();
+            let settings: Settings = serde_json::from_str(&contents).unwrap();
+            settings
         }
         Err(e) if e.kind() == ErrorKind::NotFound => {
             let settings = get_settings_path();
             let mut path = settings.clone();
             path.pop();
-            fs::create_dir(path).unwrap();
+            if fs::metadata(path.clone()).is_ok() == false {
+                fs::create_dir(path).unwrap();
+            }
             File::create(settings).unwrap();
             Settings { width: 0, height: 0 }
         },
         Err(e) => panic!("Error: {:?}", e)
     }
-    // let mut contents = String::new();
-    // file.read_to_string(&mut contents)?;
-    // assert_eq!(contents, "Hello, world!");
+   
 }
 
 pub fn save_settings(settings: &Settings) {
     let settings_path = get_settings_path();
-    let _ = File::open(settings_path).unwrap(); 
-    println!("Size {}", settings.width);
+    let json = serde_json::to_string(&settings).unwrap();
+    let mut file = File::create(settings_path).unwrap();
+    file.write(&json.into_bytes()).expect("Unable to write settings");
 }
 
 fn get_settings_path() -> PathBuf {
