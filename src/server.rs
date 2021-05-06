3d6285453d77be69e8060446bcc4d3a366f0c442
uriegel <uriegel@hotmail.de>
2021-05-06 23:10:44 +0200

    expires header, but static

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
-use warp::Filter;
+extern crate chrono;
+use chrono::Utc;
+
+use warp::{Filter, filters::reply::WithHeaders, http::HeaderValue, hyper::HeaderMap};
 use tokio::runtime::Runtime;
 
 pub fn start(rt: &Runtime, port: u16)-> () {
@@ -12,7 +15,15 @@ pub fn start(rt: &Runtime, port: u16)-> () {
                 format!("Hello, {}!", name)
             });
     
-        let route2 = warp::fs::dir(".");
+        fn expires_now() -> WithHeaders {
+            let mut headers = HeaderMap::new();
+            let now = Utc::now();
+            let now_str = now.format("%a, %d %h %Y %T GMT").to_string();
+            headers.insert("Expires", HeaderValue::from_str(now_str.as_str()).unwrap());
+            warp::reply::with::headers(headers)
+        }
+
+        let route2 = warp::fs::dir(".").with(expires_now());
         let routes = route1.or(route2);
     
         warp::serve(routes)
