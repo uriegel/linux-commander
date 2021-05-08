2a5d8a43d89f30991c30483b56b6c860989403ae
uriegel <uriegel@hotmail.de>
2021-05-08 16:59:32 +0200

    get_mountpoint

--- src/requests.rs
+++ src/requests.rs
@@ -1,4 +1,6 @@
-use block_utils::{BlockUtilsError, FilesystemType, get_block_partitions_iter, get_device_info};
+use std::path::PathBuf;
+
+use block_utils::{BlockUtilsError, FilesystemType, get_block_partitions_iter, get_device_info, get_mountpoint, is_mounted};
 use serde::{Serialize};
 
 #[derive(Serialize)]
@@ -8,24 +10,42 @@ pub struct RootItem {
     pub mount_point: String,
     pub capacity: u64,
     pub media_type: bool,
-    pub file_system: bool
+    pub file_system: bool,
+    pub is_mounted: bool
 }
 
 pub fn get_root_items()->Result<Vec<RootItem>, BlockUtilsError> {
     Ok(get_block_partitions_iter()?
-        .map(|item| match get_device_info(item) {
-            Ok(item) => Some(item),
+        .map(|path| match get_device_info(path.clone()) {
+            Ok(item) => {
+                let (mount_point, is_mounted) = 
+                    match get_mountpoint(path.clone()) {
+                        Ok(mount_point) => 
+                            match mount_point {
+                                Some(mount_point) => (mount_point.to_string_lossy().to_string(),
+                                    match is_mounted(mount_point) {
+                                        Ok(is_mounted) => is_mounted,
+                                        _ => false
+                                    }),
+                                None => (String::new(), false)
+                            }
+                            ,
+                        _ => (String::new(), false)
+                    };
+
+                Some({
+                    RootItem {
+                    name: item.name, 
+                    capacity: item.capacity,
+                    mount_point,
+                    file_system: true, //item.fs_type,
+                    media_type: false,
+                    is_mounted
+                }})
+            },
             Err(_) => None
         })
         .filter(|item| item.is_some())
         .map(|item|item.unwrap())
-        .map(| item | RootItem {
-            name: item.name, 
-            capacity: item.capacity,
-            mount_point: "mount".to_string(),
-            file_system: true, //item.fs_type,
-            media_type: false
-        })
         .collect())
-        
 }
