404965e0942945fbc7d59f7fd4c7a47a5a664d9f
uriegel <uriegel@hotmail.de>
2021-05-08 10:41:25 +0200

    async get with path params

--- src/server.rs
+++ src/server.rs
@@ -4,8 +4,13 @@ use chrono::Utc;
 use warp::{Filter, Reply, fs::File, http::HeaderValue, hyper::{Body, HeaderMap, Response}};
 use tokio::runtime::Runtime;
 
-async fn schrott()->Result<impl warp::Reply, warp::Rejection> {
-    let our_ids = vec![1, 3, 7, 13];
+async fn get_root()->Result<impl warp::Reply, warp::Rejection> {
+    let our_ids = vec![1, 3, 7, 144];
+    Ok(warp::reply::json(&our_ids))
+}
+
+async fn get_items(path: String)->Result<impl warp::Reply, warp::Rejection> {
+    let our_ids = vec![1, 3, 7, 77];
     Ok(warp::reply::json(&our_ids))
 }
 
@@ -15,11 +20,17 @@ pub fn start(rt: &Runtime, port: u16)-> () {
     rt.spawn(async move {
         println!("Running test server on http://localhost:{}", port); 
 
-        let route1 = warp::get()
+        let route_get_root = warp::get()
             .and(warp::path("commander"))
             .and(warp::path("getroot"))
             .and(warp::path::end())
-            .and_then(schrott);
+            .and_then(get_root);
+
+        let route_get_items = warp::get()
+            .and(warp::path("commander"))
+            .and(warp::path("getitems"))
+            .and(warp::path::param())
+            .and_then(get_items);
 
         fn add_headers(reply: File)->Response<Body> {
             let mut header_map = HeaderMap::new();
@@ -34,10 +45,13 @@ pub fn start(rt: &Runtime, port: u16)-> () {
             res
         }
 
-        let route2 = warp::fs::dir(".")
+        let route_static = warp::fs::dir(".")
             .map(add_headers);
 
-        let routes = route1.or(route2);
+        let routes = 
+            route_get_root
+            .or(route_get_items)
+            .or(route_static);
     
         warp::serve(routes)
             .run(([127, 0, 0, 1], port))
