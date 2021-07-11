1ba2c33970b0e4709c61c218eecd08d4b319dea7
uriegel <uriegel@hotmail.de>
2021-07-11 18:55:08 +0200

    DialogBoxComponent added

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
 import './components/gridsplitter.js'
 import './components/pdfviewer.js'
+import './components/DialogBoxComponent.js'
 import './folder.js'
 import {onTheme as onViewerTheme, onShowViewer, refreshViewer} from './viewer.js'
 
 const folderLeft = document.getElementById("folderLeft")
 const folderRight = document.getElementById("folderRight")
 const splitter = document.getElementById('splitter')
+const dialog = document.querySelector('dialog-box-component')
 
 initializeCallbacks(onTheme, onShowHidden, show => {
     onShowViewer(show, currentPath)
@@ -20,10 +22,10 @@ initializeCallbacks(onTheme, onShowHidden, show => {
 })()
 
 folderLeft.addEventListener("onFocus", evt => {
-     activeFolder = folderLeft
+    activeFolder = folderLeft
 })
 folderRight.addEventListener("onFocus", evt => {
-     activeFolder = folderRight
+    activeFolder = folderRight
 })
 
 const onPathChanged = evt => {
@@ -39,19 +41,28 @@ folderRight.addEventListener("tab", () => folderLeft.setFocus())
 folderLeft.addEventListener("delete", evt => onDelete(evt.detail))
 folderRight.addEventListener("delete", evt => onDelete(evt.detail))
 
-function onDelete(itemsToDelete) {
+async function onDelete(itemsToDelete) {
     console.log("del", itemsToDelete)
+
+    const res = await dialog.show({
+        text: "Löschen",
+        btnOk: true,
+        btnCancel: true
+    })    
+    activeFolder.setFocus()
 }
 
 function onTheme(theme) {
     ["themeAdwaita", "themeAdwaitaDark"].forEach(n => {
         document.body.classList.remove(n)
         splitter.classList.remove(n)
+        dialog.classList.remove(n) 
     })
     document.body.classList.add(theme)    
     splitter.classList.add(theme)    
     folderLeft.changeTheme(theme)
     folderRight.changeTheme(theme)
+    dialog.classList.add(theme)        
     onViewerTheme(theme)
     localStorage.setItem("theme", theme)
 }
