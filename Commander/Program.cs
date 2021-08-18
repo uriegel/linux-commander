97f04598955752bdde2cd3d14179ebb76cd933c4
uriegel <uriegel@hotmail.de>
2021-08-18 15:47:04 +0200

    MainWindow

--- Commander/Program.cs
+++ Commander/Program.cs
@@ -1,2 +1,18 @@
-﻿using System;
-Console.WriteLine("Hello World!");
+﻿using GtkDotNet;
+
+var app = new Application("de.uriegel.commander");
+app.Run(() =>
+{
+    app.RegisterResources();
+    using var builder = Builder.FromResource("/de/uriegel/commander/main_window.glade");
+
+    var window = new Window(builder.GetObject("window"));
+    var headerBar = new HeaderBar(builder.GetObject("headerbar"));
+
+    app.AddWindow(window);
+
+    window.ShowAll();
+});
+
+
+
