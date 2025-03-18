ff98ddf041d42a8c8f25a345d6fa3b517f51697d
uriegel <uriegel@hotmail.de>
2025-03-18 18:11:44 +0100

    In release version performant

--- Commander/Program.cs
+++ Commander/Program.cs
@@ -7,17 +7,14 @@ using Commander.UI;
 const string appId = "de.uriegel.commander";
 Application
     .NewAdwaita(appId)
-        .OnActivate(async app =>
-        {
-            var win = app
-                .SubClass(ManagedApplicationWindowClass.Register(p => new MainWindow(p), "mainwindow"))
-                .SubClass(new FolderViewPanedClass(p => new FolderViewPaned(p)))
-                .SubClass(new FolderViewClass())
-                .ManagedApplicationWindow()
-                .SaveBounds(appId, 600, 800);
-            await Task.Delay(50);
-            win.Show();
-        })
+        .OnActivate(app =>
+            app
+            .SubClass(ManagedApplicationWindowClass.Register(p => new MainWindow(p), "mainwindow"))
+            .SubClass(new FolderViewPanedClass(p => new FolderViewPaned(p)))
+            .SubClass(new FolderViewClass())
+            .ManagedApplicationWindow()
+            .SaveBounds(appId, 600, 800)
+            .Show())
         .Run(0, IntPtr.Zero);
 
 
