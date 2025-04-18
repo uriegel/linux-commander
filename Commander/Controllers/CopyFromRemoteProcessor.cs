cccf0c7a44db6be49c5a306c693ba045e3223df7
uriegel <uriegel@hotmail.de>
2025-04-18 11:23:59 +0200

    CopyFromRemote CreateCopyItem

--- Commander/Controllers/CopyFromRemoteProcessor.cs
+++ Commander/Controllers/CopyFromRemoteProcessor.cs
@@ -1,9 +1,15 @@
 using Commander.Enums;
+using CsTools.Extensions;
 
 namespace Commander.Controllers;
 
 class CopyFromRemoteProcessor(string sourcePath, string? targetPath, SelectedItemsType selectedItemsType, DirectoryItem[] selectedItems)
     : CopyProcessor(sourcePath, targetPath, selectedItemsType, selectedItems)
 {
-
+    protected override CopyItem? CreateCopyItem(Item source, string _, string targetPath)
+    {
+        var info = new FileInfo(targetPath.AppendPath(source.Name));
+        var target = info.Exists ? new Item(info.Name, info.Length, info.LastWriteTime) : null;
+        return new(source, target);
+    }
 }
