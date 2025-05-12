5eec471f9f9b9314852a97d0b300d1379b4ef8d6
uriegel <uriegel@hotmail.de>
2025-05-12 21:33:15 +0200

    New GtkDotNet4

--- Commander/UI/ExtendedRenameDialog.cs
+++ Commander/UI/ExtendedRenameDialog.cs
@@ -11,13 +11,13 @@ public static class ExtendedRenameDialog
         var builder = Builder.FromDotNetResource("extendedrenamedialog");
         var dialog = builder.GetWidget<AdwAlertDialogHandle>("dialog");
         var prefixText = builder.GetWidget<EntryHandle>("prefix");
-        var digits = builder.GetWidget<WidgetHandle>("digits");
-        var start = builder.GetWidget<WidgetHandle>("start");
+        var digits = builder.GetWidget<DropDownHandle>("digits");
+        var start = builder.GetWidget<SpinButtonHandle>("start");
  
         var data = Storage.Retrieve().ExtendedRenameData ?? new("Bild", 2, 1);
         prefixText.Text(data.Prefix);
-        digits.GetInternalHandle().SetSelected(data.Digits - 1);
-        start.GetInternalHandle().SetValue(data.StartIndex);
+        digits.SetSelected(data.Digits - 1);
+        start.SetValue(data.StartIndex);
 
         dialog.OnMap(async () =>
         {
@@ -28,23 +28,14 @@ public static class ExtendedRenameDialog
         if (await dialog.PresentAsync(MainWindow.MainWindowHandle) == "ok")
         {
             var prefix = prefixText.GetText() ?? data.Prefix;
-            var d = digits.GetInternalHandle().GetSelected() + 1;
-            var startVal = start.GetInternalHandle().GetValue();
+            var d = digits.GetSelected() + 1;
+            var startVal = start.GetValue();
             data = data with { Prefix = prefix, Digits = d, StartIndex = (int)startVal };
             Storage.SaveExtendedRename(data);
             
         }
         return "";
     }
-
-    [System.Runtime.InteropServices.DllImport("libgtk-4.so.1", EntryPoint = "gtk_drop_down_get_selected", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-    extern static int GetSelected(this nint misdt);
-    [System.Runtime.InteropServices.DllImport("libgtk-4.so.1", EntryPoint = "gtk_drop_down_set_selected", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-    extern static void SetSelected(this nint misdt, int pos);
-    [System.Runtime.InteropServices.DllImport("libgtk-4.so.1", EntryPoint = "gtk_spin_button_get_value", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-    extern static double GetValue(this nint misdt);
-    [System.Runtime.InteropServices.DllImport("libgtk-4.so.1", EntryPoint = "gtk_spin_button_set_value", CallingConvention = System.Runtime.InteropServices.CallingConvention.Cdecl)]
-    extern static void SetValue(this nint misdt, double value);
 }
 
 record ExtendedRenameData(
