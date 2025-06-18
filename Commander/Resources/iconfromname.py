682c344855b0f3a3f289521ce302cef00102a7aa
uriegel <uriegel@hotmail.de>
2025-06-18 22:31:29 +0200

    16px

--- Commander/Resources/iconfromname.py
+++ Commander/Resources/iconfromname.py
@@ -4,7 +4,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 import mimetypes
 import sys
-def get_icon_name(name, size=20):
+def get_icon_name(name, size=16):
     if name:
         icon = Gio.content_type_get_icon(name)
         theme = Gtk.IconTheme.get_default()
